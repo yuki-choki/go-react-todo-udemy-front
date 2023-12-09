a3992307f33f9109ea041c13a127b886ab7ad3cb
yuki-choki <ikuy0105@gmail.com>
2023-12-09 11:11:43 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# React project setup
+```bash
+npx create-react-app react-todo --template typescript --use-npm
+```
+```bash
+npm i @tanstack/react-query@4.28.0
+npm i @tanstack/react-query-devtools@4.28.0
+npm i zustand@4.3.6
+npm i @heroicons/react@2.0.16
+npm i react-router-dom@6.10.0 axios@1.3.4
+```
+https://tailwindcss.com/docs/guides/create-react-app
---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Auth } from './components/Auth'
+import { Todo } from './components/Todo'
+import axios from 'axios'
+import { CsrfToken } from './types'
+
+function App() {
+  useEffect(() => {
+    axios.defaults.withCredentials = true
+    const getCsrfToken = async () => {
+      const { data } = await axios.get<CsrfToken>(
+        `${process.env.REACT_APP_API_URL}/csrf`
+      )
+      axios.defaults.headers.common['X-CSRF-Token'] = data.csrf_token
+    }
+    getCsrfToken()
+  }, [])
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Auth />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
